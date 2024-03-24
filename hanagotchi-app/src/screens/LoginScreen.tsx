6e205287ffa20e79183099a6a3eb309bb3587834
Agustinefe <agustin.firmapaz@creativecoefficient.net>
2024-03-23 22:47:36 -0300

    Second scratch of login screen

--- hanagotchi-app/src/screens/LoginScreen.tsx
+++ hanagotchi-app/src/screens/LoginScreen.tsx
@@ -12,14 +12,16 @@ const LoginScreen: React.FC = () => {
 
     return (
         <SafeAreaView style={{...style.container, backgroundColor: theme.colors.background}}>
-            <View>
+            <View style={style.header}>
                 <Text style={{...style.title, color: theme.colors.onSecondary}}>HANAGOTCHI</Text>
                 <Text style={{...style.subtitle, color: BROWN_LIGHT}}>Tu nuevo mejor amigo</Text>
             </View>
-            <ImageBackground source={loginBackground} style={{ width: 400, height: 400 }} />
-            <Button mode="contained" uppercase style={style.button}>
-                Iniciar sesion
-            </Button>
+            <ImageBackground source={loginBackground} style={style.background} />
+            <View style={style.footer}>
+                <Button mode="contained" uppercase style={style.button} onPress={() => console.log("Press!!")}>
+                    Iniciar sesion
+                </Button>
+            </View>
         </SafeAreaView>
     )
 }
@@ -29,7 +31,22 @@ const style = StyleSheet.create({
       flex: 1,
       justifyContent: 'center',
       alignItems: "center",
-      gap: 20,
+    },
+    background: {
+        width: 400,
+        height: 400,
+    },
+    header: {
+        position: "absolute",
+        top: 100,
+        width: "100%",
+        alignItems: "center",
+    },
+    footer: {
+        position: "absolute",
+        bottom: 50,
+        width: "100%",
+        alignItems: "center",
     },
     title: {
       fontSize: 45,
@@ -43,20 +60,11 @@ const style = StyleSheet.create({
         textAlign: 'center',
         fontStyle: "italic",
       },
-    description: {
-      fontSize: 20,
-      fontFamily: "Roboto",
-      textAlign: 'center',
-      color: '#4F4C4F',
-      padding: 20,
-      width: "75%"
-    },
     button: {
       paddingHorizontal: 20,
       paddingVertical: 3,
       borderRadius: 10,
       width: "50%",
-      marginTop: 25,
     },
   })
   
