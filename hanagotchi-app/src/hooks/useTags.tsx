b8e6ca27723f454b9a1bfa87037bd1ab3ba0debd
Agustinefe <agustin.firmapaz@creativecoefficient.net>
2024-05-30 21:14:54 -0300

    Create hook to handle tag related operations

--- hanagotchi-app/src/hooks/useTags.tsx
+++ hanagotchi-app/src/hooks/useTags.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { GetSuscribedTags } from "../models/hanagotchiApi";
 import { useApiFetch } from "./useApiFetch"
 import { useHanagotchiApi } from "./useHanagotchiApi"
 
 const useTags = () => {
     const hanagotchiApi = useHanagotchiApi();
-    useApiFetch(() => hanagotchiApi.)
+    const {isFetching, fetchedData: tagList, error} = useApiFetch<GetSuscribedTags>(hanagotchiApi.getSuscribedTags, []);
+    const [tags, setTags] = useState<Set<string>>(new Set());
+    
+    useEffect(() => setTags(new Set(tagList)), [tagList])
+    
+    const subscribe = async (newTag: string) => {
+        await hanagotchiApi.subscribeToTag(newTag);
+        setTags((oldSet) => oldSet.add(newTag))
+    }
+
+    const unsubscribe = async (tag: string) => {
+        await hanagotchiApi.unsubscribeToTag(tag);
+        setTags((oldSet) => {
+            oldSet.delete(tag);
+            return oldSet;
+        })
+    }
+
+    return {
+        isFetching,
+        error,
+        tags,
+        subscribe,
+        unsubscribe,
+    }
 }
+
+export default useTags;
